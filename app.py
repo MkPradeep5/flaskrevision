899ab4e66c81d39fbde270defbb40514f74b86cd
Mk Pradeep <bmkpradeep2527@gmail.com>
2023-12-06 14:54:12 +0530

    postman

---
+++ app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, render_template,request,jsonify
+import json
+
+obj = Flask(__name__)
+
+@obj.route("/")
+def welcome():
+    return "Welcome to the flaskrevision!"
+
+@obj.route("/cal",methods=["GET"])
+def math_operator():
+    operation = request.json["operation"]
+    number1 = request.json["number1"]
+    number2 = request.json["number2"]
+
+    if operation == "add":
+        result = int(number1)+int(number2)
+    elif operation == "multiply":
+        result = int(number1)*int(number2)
+    elif operation == "division":
+        result = int(number1)/int(number2)
+    else:
+        result = int(number1)-int(number2)
+    return "The operation is {} and the result is {}".format(operation,result)
+
+if __name__== "__main__":
+    obj.run()
